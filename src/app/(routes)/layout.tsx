21cc6f94c213405f83f86bd1f3bcc719aaf33b80
peachza30 <peachza30@gmail.com>
2024-08-23 13:08:35 +0700

    add depen

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -1,26 +1,74 @@
-'use client';
-import React, { Suspense } from 'react';
-import Loading from '@/components/layout/Loading';
-import { Button } from '@mui/material';
-import { signOut } from 'next-auth/react';
+"use client";
+import { styled, Container, Box } from "@mui/material";
+import React, { useState } from "react";
+import Header from "@/components/layout/header/Header";
+import Sidebar from "@/components/layout/sidebar/Sidebar";
 
-type Props = {
+
+const MainWrapper = styled("div")(() => ({
+  display: "flex",
+  minHeight: "100vh",
+  width: "100%",
+}));
+
+const PageWrapper = styled("div")(() => ({
+  display: "flex",
+  flexGrow: 1,
+  paddingBottom: "60px",
+  flexDirection: "column",
+  zIndex: 1,
+  backgroundColor: "transparent",
+}));
+
+interface Props {
   children: React.ReactNode;
-};
+}
 
-const layout = ({ children }: Props) => {
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
+  const [isMobileSidebarOpen, setMobileSidebarOpen] = useState(false);
   return (
-    <div className='container mx-auto'>
-                <Button
-              variant="contained"
-              color="error"
-              onClick={() => signOut({ callbackUrl: '/' })}
-            >
-              Sign Out
-            </Button>
-        <Suspense fallback={<Loading />}>{children}</Suspense>
-    </div>
+    <MainWrapper className="mainwrapper">
+      {/* ------------------------------------------- */}
+      {/* Sidebar */}
+      {/* ------------------------------------------- */}
+      <Sidebar
+        isSidebarOpen={isSidebarOpen}
+        isMobileSidebarOpen={isMobileSidebarOpen}
+        onSidebarClose={() => setMobileSidebarOpen(false)}
+      />
+      {/* ------------------------------------------- */}
+      {/* Main Wrapper */}
+      {/* ------------------------------------------- */}
+      <PageWrapper className="page-wrapper">
+        {/* ------------------------------------------- */}
+        {/* Header */}
+        {/* ------------------------------------------- */}
+        <Header toggleMobileSidebar={() => setMobileSidebarOpen(true)} />
+        {/* ------------------------------------------- */}
+        {/* PageContent */}
+        {/* ------------------------------------------- */}
+        <Container
+          sx={{
+            paddingTop: "20px",
+            maxWidth: "1200px",
+          }}
+        >
+          {/* ------------------------------------------- */}
+          {/* Page Route */}
+          {/* ------------------------------------------- */}
+          <Box sx={{ minHeight: "calc(100vh - 170px)" }}>{children}</Box>
+          {/* ------------------------------------------- */}
+          {/* End Page */}
+          {/* ------------------------------------------- */}
+        </Container>
+      </PageWrapper>
+    </MainWrapper>
   );
-};
-
-export default layout;
+}
